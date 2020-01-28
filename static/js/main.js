11a3712e61bbd27ee27f2548352bb28689e2ae16
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2020-01-28 21:29:04 +0900

    完成

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
 function RollGacha() {
+    ResetReflection()
     PlaySlotSound()
     elements = document.getElementsByClassName("slot_item");
     elements = Array.from(elements);
@@ -7,26 +8,68 @@ function RollGacha() {
 
 
 function FixGacha() {
+    const item_num = 5
     for (var i = 0; i < 3; i++) {
-        rand_id = String(Math.floor(Math.random() * 4) + 1);
+        rand_id = String(Math.floor(Math.random() * item_num) + 1);
         target = document.getElementById("slot" + String(i + 1))
         if (target.src.slice(-4) == ".gif") {
             target.setAttribute("src", "/static/images/tatsuya_ex" + rand_id + ".jpg")
             if (i == 2) {
                 StopSound()
             }
+            PlayFixSound()
+            CheckGacha()
             break;
         }
     }
 }
 
-function PlaySlotSound() {
+function CheckGacha() {
+    var id_arr = [];
+    for (var i = 0; i < 3; i++) {
+        target = document.getElementById("slot" + String(i + 1))
+        if (target.src.slice(-4) == ".jpg") {
+            id_arr.push(target.src.charAt(target.src.length - 5));
+        } else {
+            return;
+        }
+    }
+    if (id_arr.every(v => v === id_arr[0])) {
+        SetReflection()
+        PlayTatsuyaScream()
+    }
+}
+
+function SetReflection() {
+    target = document.getElementById("slot")
+    target.setAttribute("class", "center reflection")
+}
+
+function ResetReflection() {
+    target = document.getElementById("slot")
+    target.setAttribute("class", "center")
+}
+
+function PlayTatsuyaScream() {
     audioElem = new Audio();
-    audioElem.loop = true;
-    audioElem.src = "/static/sounds/slot.mp3";
+    audioElem.src = "/static/sounds/scream_tatsuya.mp3";
+    audioElem.play();
+}
+
+
+function PlaySlotSound() {
+    audioElem_slot = new Audio();
+    audioElem_slot.loop = true;
+    audioElem_slot.src = "/static/sounds/slot.mp3";
+    audioElem_slot.play();
+}
+
+function PlayFixSound() {
+    audioElem = new Audio();
+    audioElem.src = "/static/sounds/fix.mp3";
     audioElem.play();
 }
 
 function StopSound() {
-    audioElem.pause();
+    audioElem_slot.pause();
 }
